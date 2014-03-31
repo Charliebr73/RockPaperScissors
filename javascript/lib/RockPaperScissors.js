a36b3b69c96826b72d85c16b168396ca6bf589c1
Charles De Barros <charliebr73@gmail.com>
2014-03-31 16:39:10 +0100

    Adding Lizzard and Spock to the game

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,3 +1,15 @@
+
+Game.prototype.Pairs = {
+rock: ["scissors", "lizard"],
+scissors: ["paper", "lizard"],
+paper: ["rock", "Spock"],
+lizard: ["Spock","paper"],
+Spock: ["scissors", "rock"]
+}
+
+
+
+
 function Player(name) {
   this.name = name;
 }
@@ -11,6 +23,19 @@ function Game(player1, player2) {
   this.player2 = player2;
 }
 
-Game.prototype.winner = function() {
-  return this.player1;
+Game.prototype.samePick = function() {
+	return this.player1.pick === this.player2.pick;
 }
+
+Game.prototype.winner = function() {
+	if(this.samePick()) return null
+	if(this.Pairs[this.player1.pick].indexOf(this.player2.pick) !== -1) {
+  	return this.player1;
+  } else {
+  	return this.player2;
+  }
+}
+
+// other option but more long-winded:
+// if(this.Pairs[this.player1.pick][0] || this.Pairs[this.player1.pick][1] === this.player2.pick {
+
